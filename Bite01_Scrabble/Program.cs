d53011a1d9392a4c07ca8472418765a29956b6cf
Lukas Reimer <lukas.reimer@icloud.com>
2019-02-24 16:13:08 +0100

    Added Scrabble class stub

--- Bite01_Scrabble/Program.cs
+++ Bite01_Scrabble/Program.cs
@@ -2,12 +2,42 @@
 
 namespace Bite01_Scrabble
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
+            var scrabble = new Scrabble();
+            Console.WriteLine(scrabble);
+
+            try
+            {
+                scrabble.LoadWords();
+            }
+            catch (NotImplementedException exception)
+            {
+                Console.WriteLine($"Exception caught: {exception.Data}\n{exception.Message}");
+            }
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+        }
+    }
+
+    class Scrabble
+    {
+        public void LoadWords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateWordValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MaximumWordValue()
+        {
+            throw new NotImplementedException();
         }
     }
 }
